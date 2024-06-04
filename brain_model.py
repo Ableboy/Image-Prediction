358c4ab92085e0e878a56e1fe73ce21b12657324
Ebube Nwoke <117966877+Ableboy@users.noreply.github.com>
2024-06-04 14:34:36 +0100

    Update brain_model.py

--- brain_model.py
+++ brain_model.py
@@ -1,7 +1,7 @@
 """import the necessary libraries"""
-from imageai.Classification import ImageClassification # The lib make prediction in classification object
+from imageai.Classification import ImageClassification # The lib make predictions in classification object
 
-import os # This brings in imgs that needs to be predicted 
+import os # This brings in imgs that need to be predicted 
 
 exec_path = os.getcwd() # This brings testable imgs to the predictive ground
 
@@ -14,6 +14,6 @@ prediction.setModelPath(os.path.join(exec_path, 'mobilenet_v2-b0353104.pth')) # 
 prediction.loadModel() # This created models runs through
 
 '''The below - predictions are the created models for testing img, while probalilities shows the assurance/accuracy of the prediction'''
-predctions, probabilities = prediction.classifyImage(os.path.join(exec_path,'godzilla.jpg'), result_count=5)
+predctions, probabilities = prediction.classifyImage(os.path.join(exec_path,'giraffe.jpg'), result_count=5)
 for eachPred, eachProb in zip(predctions, probabilities):
     print(f'{eachPred} : {eachProb}')
